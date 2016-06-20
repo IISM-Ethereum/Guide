f13c084b38d6078df5a60a2f6e0ebd8639cdb233
mgsgde <mgsgde@gmail.com>
2016-06-20 18:24:09 +0200

    alternative external calls works

--- sonstiges/EtherExchange/test2006.sol
+++ sonstiges/EtherExchange/test2006.sol
@@ -1,4 +1,5 @@
 
+
   contract Etherex {
       Market[] markets;
 
@@ -52,63 +53,61 @@
 
       uint256 _id;
 
-      Token token;
 
+      Token public token;
       // todo parameter
       // todo auf standard prüfen ?!
         function test_add_market(address addr) returns (uint256 rv){
 
-           token = Token(addr);
-           bytes32 name = "test_name1";
-           uint256 decimals = 2;
-           uint256 precision = 2;
-           uint256 minimum = 2;
-           uint256 category = 1;
 
-          address con = addr;
           _id = last_market + 1;
 
-          if (name <= 0) throw;
-          if (markets_name[name] != 0) throw;
-          if (con == 0) throw;
-          if (category < 0 || precision < 0 || minimum < 0) throw;
+          //if (name <= 0) throw;
+         // if (markets_name[name] != 0) throw;
+          //if (con == 0) throw;
+         // if (category < 0 || precision < 0 || minimum < 0) throw;
 
 
             Market storage m;
             m.id = _id;
-            m.name = name;
-            m.con = con;
-            m.category = category;
-            m.decimals = decimals;
-            m.precision = precision;
-            m.minimum = minimum;
+            m.name = "name";
+            m.con = addr;
+
             m.last_price = 1;
             m.lowest_ask_id = 0;
             m.owner = msg.sender;
             m.block = block.number;
             markets.push(m);
 
-            markets_id[con] = _id;
-            markets_name[name] = _id;
             last_market = _id;
+              settoken(addr);
             return _id;
         }
 
 
+    address public testaddress = this;
+
+    function settoken(address addr){
+      token = Token(addr);
+    }
+
+    function transferFrom() returns (uint256 rv){
+      token.transferFrom(msg.sender, this, 10);
+      return token.balanceOf(this);
+    }
+
   // überträgt tokens von deiner addresse zu diesem contract der diese dann in balances mapping speichert. damit kann dann getraded weren
-      function deposit() returns (address rv){  // parameter fesdtelgen
+      function deposit() returns (uint256 rv){  // parameter fesdtelgen
           uint256  amount = 10;
           uint256  market_id = 1;
-          address  con = markets[market_id].con;
-          address  hier = this;
-          address  sender = msg.sender;
 
-          // hier die funktion via Token.at() aufrufen um ein bool rückgabe wert zu erhalten TODO
-          con.call(bytes4(bytes32(sha3("transferFrom(address,address,uint256)"))), sender, hier, amount);
-          uint256 balance = balances[msg.sender][market_id].available;
-          balance = balance + amount;
-          balances[msg.sender][market_id].available = balance;
-          return con;
+
+          if (token.transferFrom(msg.sender, this, amount)){
+            uint256 balance = balances[msg.sender][market_id].available;
+            balance = balance + amount;
+            balances[msg.sender][market_id].available = balance;
+            return token.balanceOf(this);
+          }
       }
 
   function check_trade(uint256 amount, uint256 price, uint256 market_id) returns (bool rv){
@@ -445,5 +444,83 @@
     }
     return string(bytesString);
 }
+
+  }
+
+
+
+  contract Token {
+
+    address public token = this;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+      function transfer(address _to, uint256 _value) returns (bool success) {
+          //Default assumes totalSupply can't be over max (2^256 - 1).
+          //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+          //Replace the if with this one instead.
+          //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+          if (balances[msg.sender] >= _value && _value > 0) {
+              balances[msg.sender] -= _value;
+              balances[_to] += _value;
+              Transfer(msg.sender, _to, _value);
+              return true;
+          } else { return false; }
+      }
+
+      function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+          //same as above. Replace this line with the following if you want to protect against wrapping uints.
+          //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+          if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+              balances[_to] += _value;
+              balances[_from] -= _value;
+              allowed[_from][msg.sender] -= _value;
+              //Transfer(_from, _to, _value);
+              return true;
+          } else { return false; }
+      }
+
+      function balanceOf(address _owner) constant returns (uint256 balance) {
+          return balances[_owner];
+      }
+
+      function approve(address _spender, uint256 _value) returns (bool success) {
+          allowed[msg.sender][_spender] = _value;
+          Approval(msg.sender, _spender, _value);
+          return true;
+      }
+
+      function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+      }
+
+      mapping (address => uint256) balances;
+      mapping (address => mapping (address => uint256)) allowed;
+      uint256 public totalSupply;
+
+
+      function () {
+          //if ether is sent to this address, send it back.
+          throw;
+      }
+
+      /* Public variables of the token */
+
+      /*
+      NOTE:
+      The following variables are OPTIONAL vanities. One does not have to include them.
+      They allow one to customise the token contract & in no way influences the core functionality.
+      Some wallets/interfaces might not even bother to look at this information.
+      */
+      string public name;                   //fancy name: eg Simon Bucks
+      uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+      string public symbol;                 //An identifier: eg SBX
+      string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
+
+      function Token() {
+          balances[msg.sender] = 10000;               // Give the creator all initial tokens
+          totalSupply = 10000;                        // Update total supply
+      }
 
   }
